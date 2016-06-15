b3e69f48186b596a05b8662a781f0b27f9da0ed6
victor.fomin <victor.fomin@telespazio.com>
2016-06-15 17:14:48 +0200

    * Add examples of using py-faster-rcnn test
    * Update TODO

---
+++ examples/example_py-faster-rcnn_test.py
@@ -0,0 +1,124 @@
+
+#
+# Example : test py-faster-rcnn with trained model on images
+#
+
+import os
+from glob import glob
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Py Faster RCNN
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+
+# Py Caffe
+import caffe
+import cv2
+
+
+#### Setup the config :
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+NETS = {'vgg16': ('VGG16',
+                  'VGG16_faster_rcnn_final.caffemodel'),
+        'zf': ('ZF',
+               'ZF_faster_rcnn_final.caffemodel')}
+
+cfg.TEST.HAS_RPN = True
+
+net = 'vgg16'
+
+prototxt = os.path.join(cfg.MODELS_DIR, NETS[net][0], 'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models', NETS[net][1])
+
+if not os.path.isfile(caffemodel):
+    raise IOError('{:s} not found'.format(caffemodel))
+
+caffe.set_mode_cpu()
+
+net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+print '\n\nLoaded network {:s}'.format(caffemodel)
+
+
+def vis_detections(im, class_name, dets, thresh=0.5):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    if len(inds) == 0:
+        return
+
+    im = im[:, :, (2, 1, 0)]
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.imshow(im, aspect='equal')
+    for i in inds:
+        bbox = dets[i, :4]
+        score = dets[i, -1]
+
+        ax.add_patch(
+            plt.Rectangle((bbox[0], bbox[1]),
+                          bbox[2] - bbox[0],
+                          bbox[3] - bbox[1], fill=False,
+                          edgecolor='red', linewidth=3.5)
+        )
+        ax.text(bbox[0], bbox[1] - 2,
+                '{:s} {:.3f}'.format(class_name, score),
+                bbox=dict(facecolor='blue', alpha=0.5),
+                fontsize=14, color='white')
+
+    ax.set_title(('{} detections with '
+                  'p({} | box) >= {:.1f}').format(class_name, class_name,
+                                                  thresh),
+                 fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
+
+
+def demo(net, image_path):
+    """Detect object classes in an image using pre-computed object proposals."""
+
+    # Load the demo image
+    im = cv2.imread(image_path)
+
+    # Detect all object classes and regress object bounds
+    started = time()
+    scores, boxes = im_detect(net, im)
+    elapsed = time() - started
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(elapsed, boxes.shape[0])
+
+    # Visualize detections for each class
+    CONF_THRESH = 0.8
+    NMS_THRESH = 0.3
+    for cls_ind, cls in enumerate(CLASSES[1:]):
+        cls_ind += 1 # because we skipped background
+        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_scores = scores[:, cls_ind]
+        dets = np.hstack((cls_boxes,
+                          cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = nms(dets, NMS_THRESH)
+        dets = dets[keep, :]
+        vis_detections(im, cls, dets, thresh=CONF_THRESH)
+
+
+test_files = glob("../input/train/c0/*.jpg")
+
+counter = 3
+files = test_files[:counter]
+
+for f in files:
+
+    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    print 'Demo for {}'.format(f)
+
+    demo(net, f)
+
+plt.show()
