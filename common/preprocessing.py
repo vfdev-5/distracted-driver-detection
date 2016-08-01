19891cd54902381f4f0ffc940911e0700c111633
vfdev <vfdev.5@gmail.com>
2016-08-01 17:41:42 +0200

    Update preprocessing.py

--- common/preprocessing.py
+++ common/preprocessing.py
@@ -4,6 +4,7 @@
 
 # Python
 import os
+import platform
 import multiprocessing
 import itertools
 
@@ -22,23 +23,60 @@ def preprocess_image(in_image, w, h):
 
 
 def read_image(filename, size):
+    """
+    Read image from file
+    :param filename: path to image
+    :param size: list of width and height, e.g [100, 120]
+    :return: ndarray with image data. ndarray.shape = (height, widht, number of channels)
+    """
     assert os.path.exists(filename), "Image path '%s' from file list is not found" % filename
-    img = cv2.imread(filename)
-    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    assert len(size) == 2 and size[0] > 0 and size[1] > 0, "Size should be a list of 2 positive values" 
+    img = cv2.imread(filename) # img.shape = (H, W, Ch)
+    if len(img.shape) == 3 and img.shape[2] == 3: # if image has 3 channels
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    elif len(img.shape) == 2:
+        img = img.reshape(img.shape + (1,))
     if size is not None:
-        rgb = cv2.resize(rgb, (size[0], size[1]))
-    return rgb
+        img = cv2.resize(img, (size[0], size[1]))
+    return img
+    
+
+def _get_raw_data(files, width, height):
+    out = None
+    for i, f in enumerate(files):
+        data = read_image(f, (width, height))
+        if out is None:
+            k = data.shape[2]
+            out = np.empty((len(files), k, height, width), dtype=np.uint8)
+        out[i, :, :, :] = data[:, :, :].T
+    return out
 
 
-# def get_raw_data(files, width, height):
-#     out = None
-#     for i, f in enumerate(files):
-#         data = read_image(f, (width, height))
-#         if out is None:
-#             out = np.empty((len(files), ))
-#
-#         out[i, :, :, :] = data[:, :, :]
-#     return out
+def _open_file(args):
+    f = args[0]
+    size = args[1]
+    data = read_image(f, (size[0], size[1]))
+    return data
+
+
+def _get_raw_data_parallel(files, width, height):
+    # define output image shape from the first image:
+    n_workers = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=n_workers)
+    out = pool.map(_open_file, itertools.izip(files, itertools.repeat((width, height))))
+    pool.close()
+    out = np.array(out)
+    out = out.transpose((0, 3, 2, 1))
+    return out
+
+
+def get_raw_data(files, width, height):
+    
+    # Do not use multiprocessing on MacOSX
+    if "Darwin" not in platform.system():
+        return _get_raw_data_parallel(files, widht, height)
+    else:
+        return _get_raw_data(files, widht, height)
 
 
 def get_data2(files, width, height):
